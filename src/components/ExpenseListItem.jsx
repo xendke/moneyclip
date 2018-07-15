c78811b87db161fa35fcde127905f8319860799c
Xavier Gomez <xendke@gmail.com>
2018-07-14 20:00:25 -0400

    show amount as dollars instead of cents

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -7,7 +7,7 @@ const ExpenseListItem = (props) => (
 		<Link to={`/edit/${props.id}`}>
 			<h4>{props.description}</h4>
 		</Link>
-		<p>{props.amount} - {moment(props.createdAt).format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
+		<p>${(props.amount / 100).toFixed(2)} - {moment(props.createdAt).format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
 	</div>
 );
 
