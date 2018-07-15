e896239320e1180a45c018dbfcdc9359eff68325
Xavier Gomez <xendke@gmail.com>
2018-07-14 20:00:03 -0400

    moving tests

--- src/components/ExpenseDashboardPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ExpenseDashboardPage from './ExpenseDashboardPage';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
