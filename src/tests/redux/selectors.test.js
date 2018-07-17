f952b450d87bb5f31919c3505475481434d2e669
Xavier Gomez <xendke@gmail.com>
2018-07-17 11:59:33 -0400

    share expenses mock data

--- src/tests/redux/selectors.test.js
+++ src/tests/redux/selectors.test.js
@@ -1,96 +1,61 @@
 import getVisibleExpenses from '../../redux/selectors';
 import moment from 'moment';
 
+const expenses = [{
+	id: '1',
+	description: "the quick brown fox jumped over",
+	note: '',
+	amount: 100,
+	createdAt: -100
+}, {
+	id: '2',
+	description: "the lazy dog",
+	note: '',
+	amount: 300,
+	createdAt: 5
+}, {
+	id: '3',
+	description: "expense here",
+	note: '',
+	amount: 800,
+	createdAt: 100
+}];
+
 it('matches text (one word)', () => {
-	const creationMoment = moment().valueOf();
-	const expenses = [{
-		description: "the quick jumped Expected quick brown fox",
-		createdAt: creationMoment
-	}];
 	const filters = {
-		text: "exPected",
+		text: "tHe",
 		sortBy: "date",
-		startDate: moment().startOf('month'),
-		endDate: moment().endOf('month')
+		startDate: undefined,
+		endDate: undefined
 	};
 	const visibleExpenses = getVisibleExpenses(expenses, filters);
-	expect(visibleExpenses).toMatchObject(expenses);
+	const expected = [expenses[1], expenses[0]];
+
+	expect(visibleExpenses).toMatchObject(expected);
 });
 
 it('sorts by date', () => {
-	const expenses = [{
-		description: "1",
-		createdAt: moment(100).valueOf()
-	}, {
-		description: "2",
-		createdAt: moment(200).valueOf()
-	}, {
-		description: "3",
-		createdAt: moment(300).valueOf()
-	}];
-
-	const expected = [{
-		description: "3",
-		createdAt: moment(300).valueOf()
-	}, {
-		description: "2",
-		createdAt: moment(200).valueOf()
-	},
-	{
-		description: "1",
-		createdAt: moment(100).valueOf()
-	}]
 	const filters = {
 		text: "",
 		sortBy: "date",
 		startDate: undefined,
 		endDate: undefined
 	};
+	const expected = [expenses[2], expenses[1], expenses[0]];
 	const visibleExpenses = getVisibleExpenses(expenses, filters);
+
 	expect(visibleExpenses).toMatchObject(expected);
 });
 
 it('sorts by amount', () => {
-	const creationMoment = moment().valueOf();
-	const expenses = [
-		{
-			description: "1",
-			amount: 100,
-			createdAt: creationMoment
-		}, {
-			description: "3",
-			amount: 300,
-			createdAt: creationMoment
-		}, {
-			description: "2",
-			amount: 200,
-			createdAt: creationMoment
-		},
-	];
-
-	const expected = [
-		{
-			description: "3",
-			amount: 300,
-			createdAt: creationMoment
-		}, {
-			description: "2",
-			amount: 200,
-			createdAt: creationMoment
-		},
-		{
-			description: "1",
-			amount: 100,
-			createdAt: creationMoment
-		}
-	];
-
 	const filters = {
 		text: "",
 		sortBy: "amount",
 		startDate: undefined,
 		endDate: undefined
 	};
+	const expected = [expenses[2], expenses[1], expenses[0]];
 	const visibleExpenses = getVisibleExpenses(expenses, filters);
+
 	expect(visibleExpenses).toEqual(expected);
 });
