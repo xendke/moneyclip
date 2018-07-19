a15b27f3dc265e7915b1248fef9eb8c1e0dfd04a
Xavier Gomez <xendke@gmail.com>
2018-07-18 21:31:26 -0400

    finish testing redux selector

--- src/tests/redux/selectors.test.js
+++ src/tests/redux/selectors.test.js
@@ -6,25 +6,37 @@ const expenses = [{
 	description: "the quick brown fox jumped over",
 	note: '',
 	amount: 100,
-	createdAt: -100
+	createdAt: moment(0).valueOf()
 }, {
 	id: '2',
 	description: "the lazy dog",
 	note: '',
-	amount: 300,
-	createdAt: 5
+	amount: 1000,
+	createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
 	id: '3',
 	description: "expense here",
 	note: '',
 	amount: 800,
-	createdAt: 100
+	createdAt: moment(0).add(5, 'days').valueOf()
 }];
 
 it('matches text (one word)', () => {
 	const filters = {
-		text: "tHe",
-		sortBy: "date",
+		text: "the",
+		sortBy: "amount",
+		startDate: undefined,
+		endDate: undefined
+	};
+	const visibleExpenses = getVisibleExpenses(expenses, filters);
+	const expected = [expenses[1], expenses[0]];
+
+	expect(visibleExpenses).toMatchObject(expected);
+});
+it('matches text (capitalized)', () => {
+	const filters = {
+		text: "The",
+		sortBy: "amount",
 		startDate: undefined,
 		endDate: undefined
 	};
@@ -41,7 +53,7 @@ it('sorts by date', () => {
 		startDate: undefined,
 		endDate: undefined
 	};
-	const expected = [expenses[2], expenses[1], expenses[0]];
+	const expected = [expenses[2], expenses[0], expenses[1]];
 	const visibleExpenses = getVisibleExpenses(expenses, filters);
 
 	expect(visibleExpenses).toMatchObject(expected);
@@ -54,7 +66,33 @@ it('sorts by amount', () => {
 		startDate: undefined,
 		endDate: undefined
 	};
-	const expected = [expenses[2], expenses[1], expenses[0]];
+	const expected = [expenses[1], expenses[2], expenses[0]];
+	const visibleExpenses = getVisibleExpenses(expenses, filters);
+
+	expect(visibleExpenses).toEqual(expected);
+});
+
+it('filters by startDate', () => {
+	const filters = {
+		text: '',
+		sortBy: 'date',
+		startDate: moment(0),
+		endDate: undefined
+	}
+	const expected = [expenses[2], expenses[0]];
+	const visibleExpenses = getVisibleExpenses(expenses, filters);
+
+	expect(visibleExpenses).toEqual(expected);
+});
+
+it('filters by endDate', () => {
+	const filters = {
+		text: '',
+		sortBy: 'date',
+		startDate: undefined,
+		endDate: moment(0)
+	}
+	const expected = [expenses[0], expenses[1]];
 	const visibleExpenses = getVisibleExpenses(expenses, filters);
 
 	expect(visibleExpenses).toEqual(expected);
