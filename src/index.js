5a49cb80b4ea7c2e1e0b58dbb261f36e82d383f6
Xavier Gomez <xendke@gmail.com>
2018-07-26 21:38:13 -0400

    cleaning up

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
-import addMockExpenses from './utils/mockStoreData';
 
 const store = configureStore();
-
-addMockExpenses(store);
 
 const wrappedApp = (
 	<Provider store={store}>
