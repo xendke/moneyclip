d487559821b6074809f5adb6628fd1509c2a0174
Xavier Gomez <xendke@gmail.com>
2018-07-30 22:04:01 -0400

    test for multiple expenses

--- src/tests/components/ExpensesSummary.test.jsx
+++ src/tests/components/ExpensesSummary.test.jsx
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
 
 
-it('renders correctly given data', () => {
+it('renders correctly given data (single expense)', () => {
 	const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={1200} />);
+	expect(wrapper).toMatchSnapshot();
+});
+
+it('renders correctly given data (multiple expenses)', () => {
+	const wrapper = shallow(<ExpensesSummary expenseCount={2} expensesTotal={1200} />);
 	expect(wrapper).toMatchSnapshot();
 });
 
